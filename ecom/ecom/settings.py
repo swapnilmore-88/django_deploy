7eb255d0f29559692e2f35908d9c20e5096324ba
swapnilmore-88 <swapnil.more061@gmail.com>
2021-12-07 18:59:38 +0530

    removed mysql database

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -81,14 +81,6 @@ DATABASES = {
     'sqlt': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': 'Mech123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
     }
 }
 
