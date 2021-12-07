b0b9a4f3d43fec8d0dde0656d3e340e251f08751
swapnilmore-88 <swapnil.more061@gmail.com>
2021-12-07 18:29:16 +0530

    modified in settimgs

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b-@o-r)$=cn-q=j)$ioao4kjq1fxsff(t)98vs^270g38(fbvt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
